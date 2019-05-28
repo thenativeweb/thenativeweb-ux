b5f7ed088ca3f8662c6d2165269a60e03de593bc
Matthias Wagler <wagler@intuity.de>
2019-05-28 21:42:16 +0200

    Introduce themes.extend function to create custom themes.

---
+++ examples/with-next-js/pages/themes.jsx
@@ -0,0 +1,26 @@
+import CustomComponent from '../components/CustomComponent.jsx';
+import React from 'react';
+import {
+  ThemeProvider,
+  themes,
+  Website
+} from 'thenativeweb-ux';
+
+const customTheme = themes.extend('thenativeweb', {
+  components: {
+    CustomComponent: {
+      width: 42
+    }
+  }
+});
+
+const Page = () => (
+  <ThemeProvider theme={ customTheme }>
+    <Website>
+      <header>Themes</header>
+      <CustomComponent id='custom-component' size='lg'>This is a custom component</CustomComponent>
+    </Website>
+  </ThemeProvider>
+);
+
+export default Page;
--- src/themes/index.js
+++ src/themes/index.js
@@ -1,7 +1,9 @@
+import extend from './extend';
 import thenativeweb from './thenativeweb';
 import wolkenkit from './wolkenkit';
 
 export default {
+  extend,
   thenativeweb,
   wolkenkit
 };
