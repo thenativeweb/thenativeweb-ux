1bbb11913d0b253a1f16dc20e0d0e3f4a294f864
Matthias Wagler <wagler@intuity.de>
2019-10-04 09:38:12 +0200

    Adjust ControlGroup export scheme.

--- lib/components/ControlGroup/index.tsx
+++ lib/components/ControlGroup/index.tsx
@@ -1,9 +1,10 @@
-import ControlGroup from './ControlGroupDefault';
+import ControlGroupDefault from './ControlGroupDefault';
 import ControlGroupDivider from './ControlGroupDivider';
 import ControlGroupItem from './ControlGroupItem';
 
+export default ControlGroupDefault;
+
 export {
-  ControlGroup,
   ControlGroupItem,
   ControlGroupDivider
 };
