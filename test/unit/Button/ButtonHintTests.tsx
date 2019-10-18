90eb74d52bde0b08344d2fe45e228494ac612c2b
Valentina Rotkin <valentina.rotkin@thenativeweb.io>
2019-10-18 13:10:39 +0200

    Add ButtonHint test.

---
+++ test/unit/Button/ButtonHintTests.tsx
@@ -0,0 +1,35 @@
+import act from '../../shared/act';
+import assert from 'assertthat';
+import Button from '../../../lib/components/Button/ButtonDefault';
+import ButtonHint from '../../../lib/components/Button/ButtonHint';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ThemeProvider from '../../../lib/components/ThemeProvider';
+
+suite('ButtonHint', (): void => {
+  let container: Element;
+
+  setup((): void => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  teardown((): void => {
+    document.body.removeChild(container);
+  });
+
+  test('sets ButtonHint class.', async (): Promise<void> => {
+    act((): void => {
+      ReactDOM.render(
+        <ThemeProvider>
+          <ButtonHint>Click me</ButtonHint>
+        </ThemeProvider>,
+        container
+      );
+    });
+
+    const buttonHint = container.getElementsByTagName('span')[0];
+
+    assert.that(buttonHint.className).is.containing('ButtonHint');
+  });
+});
