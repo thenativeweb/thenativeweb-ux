71f84bc92d6cd49efad2774b033bdb8ec33fedee
Golo Roden <golo.roden@thenativeweb.io>
2019-10-20 14:32:23 +0200

    Fix integration tests (#29)
    * Fix integration tests.
    
    * Fix eslint errors and typescript warnings.
    
    * Move CircleCI build to VM, to make Docker work.
    
    * Clear cache.

--- test/integration/pre.ts
+++ test/integration/pre.ts
@@ -1,83 +1,44 @@
+import nginx from '../shared/containers/nginx';
 import path from 'path';
 import shell from 'shelljs';
-import { ChildProcess, spawn } from 'child_process';
 
 /* eslint-disable @typescript-eslint/no-floating-promises */
 (async (): Promise<void> => {
-  try {
-    const projectRoot = path.join(__dirname, '..', '..'),
-          webpackExampleRoot = path.join(projectRoot, 'examples', 'with-webpack');
+  const projectRoot = path.join(__dirname, '..', '..');
+  const nextJsExampleRoot = path.join(projectRoot, 'examples', 'with-next-js');
 
-    shell.rm('-rf', path.join(projectRoot, 'dist'));
+  // Remove temporary build folder from previous tests.
+  shell.rm('-rf', [
+    path.join(projectRoot, 'build')
+  ]);
 
-    let childProcess = shell.exec('npx roboter build', { cwd: projectRoot });
+  // Create a build via roboter, so
+  // that the test application always uses the the latest version.
+  let childProcess = shell.exec('npx roboter build', { cwd: projectRoot });
+
+  if (childProcess.code !== 0) {
+    throw new Error('Failed to create build.');
+  }
+
+  // Install the depencencies of the Next.js example.
+  if (!shell.test('-d', path.join(nextJsExampleRoot, 'node_modules'))) {
+    childProcess = shell.exec('npm install', { cwd: nextJsExampleRoot });
 
     if (childProcess.code !== 0) {
-      throw new Error('Failed to create dist.');
+      throw new Error('Failed to install example dependencies.');
     }
-
-    // We install the depencencies of the webpack example.
-    if (!shell.test('-d', path.join(webpackExampleRoot, 'node_modules'))) {
-      childProcess = shell.exec('npm install', { cwd: webpackExampleRoot });
-
-      if (childProcess.code !== 0) {
-        throw new Error('Failed to install example dependencies.');
-      }
-    }
-
-    await new Promise((resolve): void => {
-      let serverOutput: string;
-      let devServerProcess: ChildProcess | undefined,
-          logErrors: (err: Error) => void,
-          watchServerStart: (data: any) => void;
-
-      const cleanUpOnExit = function (): void {
-        if (devServerProcess) {
-          devServerProcess.stdout!.removeListener('data', watchServerStart);
-          devServerProcess.stderr!.removeListener('data', logErrors);
-
-          process.kill(-devServerProcess.pid);
-          devServerProcess = undefined;
-        }
-      };
-
-      logErrors = (err): void => {
-        /* eslint-disable no-console */
-        console.log(err.toString());
-        /* eslint-enable no-console */
-      };
-
-      watchServerStart = (data): void => {
-        serverOutput += data;
-
-        /* eslint-disable no-console */
-        console.log(data.toString('utf8'));
-        /* eslint-enable no-console */
-
-        if (serverOutput.includes('Compiled successfully.')) {
-          if (devServerProcess) {
-            devServerProcess.stdout!.removeListener('data', watchServerStart);
-          }
-
-          return resolve();
-        }
-      };
-
-      devServerProcess = spawn('npm', [ 'run', 'serve' ], {
-        detached: true,
-        cwd: webpackExampleRoot
-      });
-      devServerProcess.stdout!.on('data', watchServerStart);
-      devServerProcess.stderr!.on('data', logErrors);
-
-      process.once('exit', cleanUpOnExit);
-      process.once('SIGINT', cleanUpOnExit);
-      process.once('SIGTERM', cleanUpOnExit);
-    });
-  } catch (ex) {
-    /* eslint-disable no-console */
-    console.log(ex);
-    /* eslint-enable no-console */
   }
+
+  // Build a static export of the Next.js example that we can then serve
+  // using nginx. If we would run the Next.js app as a server,
+  // Next.js compiles pages lazily. This would result in higher and unpredicatable
+  // timeouts for the tests.
+  childProcess = shell.exec('npm run export', { cwd: nextJsExampleRoot });
+
+  if (childProcess.code !== 0) {
+    throw new Error('Failed to create static export from Next.js example.');
+  }
+
+  await nginx.start();
 })();
 /* eslint-enable @typescript-eslint/no-floating-promises */
