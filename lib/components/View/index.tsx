bb05bbc00fcf0f9fad1e5cf3e2cb0f97893a0161
Valentina Rotkin <valentina.rotkin@thenativeweb.io>
2019-10-21 12:22:26 +0200

    Set property to boolean.

--- lib/components/View/index.tsx
+++ lib/components/View/index.tsx
@@ -11,7 +11,7 @@ interface ViewProps {
   id?: string;
   justifyContent?: 'center';
   orientation?: 'horizontal' | 'vertical' | 'centered';
-  isScrollable?: true | false;
+  isScrollable?: boolean;
   style?: any;
 }
 
