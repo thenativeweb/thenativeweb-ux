3b34e21c7dadee623e50b8d0fd0d4aee41590104
Valentina Rotkin <valentina.rotkin@thenativeweb.io>
2019-10-24 12:44:24 +0200

    Add first tests.

---
+++ test/unit/Dropdown/DropdownTests.tsx
@@ -0,0 +1,44 @@
+import act from '../../shared/act';
+import assert from 'assertthat';
+import Dropdown, { DropdownOption } from '../../../lib/components/Dropdown';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ThemeProvider from '../../../lib/components/ThemeProvider';
+
+suite('Dropdown', (): void => {
+  let container: Element;
+
+  setup((): void => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  teardown((): void => {
+    document.body.removeChild(container);
+  });
+
+  test('renders although only required properties has been defined.', async (): Promise<void> => {
+    const options = [
+      {
+        label: 'random-label',
+        value: 'some-value'
+      }, {
+        label: 'some-label',
+        value: 'random-value'
+      }];
+
+    act((): void => {
+      ReactDOM.render(
+        <ThemeProvider>
+          <Dropdown options={ options } value='some-value'>Dropdown</Dropdown>
+        </ThemeProvider>,
+        container
+      );
+    });
+
+    const dropdown = container.querySelector('div');
+
+    assert.that(dropdown).is.not.null();
+    assert.that(dropdown!.className).is.containingAllOf(['Dropdown', 'SizeMd']);
+  });
+});
