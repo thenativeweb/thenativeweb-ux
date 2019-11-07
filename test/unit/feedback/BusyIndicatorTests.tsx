6fd8553dadacae2c133ce8456a7915ac425c3162
Valentina Rotkin <49920652+vaalllaa@users.noreply.github.com>
2019-11-07 13:41:05 +0100

    Add unit tests for BusyIndicator component and move to category feedback. (#83)

---
+++ test/unit/feedback/BusyIndicatorTests.tsx
@@ -0,0 +1,48 @@
+import act from '../../shared/act';
+import assert from 'assertthat';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BusyIndicator, ThemeProvider } from '../../../lib';
+
+suite('BusyIndicator', (): void => {
+  let container: Element;
+
+  setup((): void => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  teardown((): void => {
+    document.body.removeChild(container);
+  });
+
+  test('renders although no property has been defined.', async (): Promise<void> => {
+    act((): void => {
+      ReactDOM.render(
+        <ThemeProvider>
+          <BusyIndicator />
+        </ThemeProvider>,
+        container
+      );
+    });
+
+    const busy = container.querySelector('div');
+
+    assert.that(busy!.className).is.containing('BusyIndicator');
+  });
+
+  test('returns null if property isVisible is set to false.', async (): Promise<void> => {
+    act((): void => {
+      ReactDOM.render(
+        <ThemeProvider>
+          <BusyIndicator isVisible={ false } />
+        </ThemeProvider>,
+        container
+      );
+    });
+
+    const busy = container.querySelector('div');
+
+    assert.that(busy).is.null();
+  });
+});
