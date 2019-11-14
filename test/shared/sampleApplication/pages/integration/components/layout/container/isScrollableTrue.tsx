a31f471ff4426458aa48b87954ef3e854f711565
Matthias Wagler <wagler@intuity.de>
2019-11-14 19:22:58 +0100

    Change colors used integration tests. (#101)

--- test/shared/sampleApplication/pages/integration/components/layout/container/isScrollableTrue.tsx
+++ test/shared/sampleApplication/pages/integration/components/layout/container/isScrollableTrue.tsx
@@ -6,10 +6,10 @@ import React, { ReactElement } from 'react';
 export default (): ReactElement => (
   <div>
     <Container id='container' vertical='center' horizontal='center' isScrollable={ true }>
-      <div style={{ background: 'gold', width: 400 }}>
-        <div style={{ background: 'red', height: 20 }} />
-        <div style={{ background: 'gold', height: 400 }} />
-        <div style={{ background: 'green', height: 20 }} />
+      <div style={{ width: 400 }}>
+        <div style={{ background: 'deeppink', height: 20 }} />
+        <div style={{ background: 'gray', height: 400 }} />
+        <div style={{ background: 'deeppink', height: 20 }} />
       </div>
     </Container>
   </div>
