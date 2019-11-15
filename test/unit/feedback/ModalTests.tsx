92d34a53f3e01ad36612691be8a73ddd859d808d
Golo Roden <golo.roden@thenativeweb.io>
2019-11-15 15:47:07 +0100

    Update dependencies.

--- test/unit/feedback/ModalTests.tsx
+++ test/unit/feedback/ModalTests.tsx
@@ -1,5 +1,5 @@
 import { act } from '../../shared/act';
-import assert from 'assertthat';
+import { assert } from 'assertthat';
 import { click } from '../../shared/eventDispatchers';
 import { getPortalRootNode } from '../../../lib/services/getPortalRootNode';
 import React from 'react';
