58b8aff5467445ff13ca8fe76ed20287d3569270
Matthias Wagler <wagler@intuity.de>
2019-11-15 07:30:09 +0100

    Fix Dialogs test.

--- test/unit/feedback/DialogsTests.tsx
+++ test/unit/feedback/DialogsTests.tsx
@@ -23,7 +23,7 @@ suite('Dialogs', (): void => {
     document.body.removeChild(portalRootNode);
   });
 
-  test.only('renders dialogs into portal root node.', async (): Promise<void> => {
+  test('renders dialogs into portal root node.', async (): Promise<void> => {
     act((): void => {
       ReactDOM.render(
         <ThemeProvider>
@@ -33,14 +33,10 @@ suite('Dialogs', (): void => {
       );
     });
 
-    // TODO: We need to fix this test. Currently it simply hangs without a
-    // timeout and results in a printed stack trace "JavaScript heap out of
-    // memory" which seems like we're
-    // causing an infinite loop here.
     await Promise.all([
       new Promise((resolve, reject): void => {
-        // This promise will only resolve if somebody clicked on the
-        // confirm action. So we might need to change the API.
+        // This promise only resolves if the confirm button inside the dialog
+        // has been clicked.
         dialogs.confirm({
           title: 'Do you want to proceed?',
           actions: {
@@ -56,24 +52,21 @@ suite('Dialogs', (): void => {
           catch(reject);
       }),
       new Promise((resolve): void => {
-        // We delay the following code so the confirm dialog can be rendered.
+        // We delay the following code so the confirm dialog can be rendered
+        // first.
         setTimeout((): void => {
           const headerText = portalRootNode.querySelector<HTMLElement>('.Dialogs-for-testing-1 [class^=Headline]');
 
-          // This assertion succeeds
-          assert.that(headerText).is.equalTo('Do you want to proceed?');
+          assert.that(headerText!.textContent).is.equalTo('Do you want to proceed?');
 
           const confirmButton = portalRootNode.querySelector<HTMLElement>('#dialogs-confirm-action-confirm');
-
-          // This line never seems to be reached
-          console.log(confirmButton!.innerHTML);
 
           act((): void => {
             click(confirmButton!);
           });
 
           resolve();
-        }, 500);
+        }, 100);
       })
     ]);
   });
