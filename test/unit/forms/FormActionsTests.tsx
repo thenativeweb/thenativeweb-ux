3f5addf22bce14787de80b1fc5bf5527a86104ea
Valentina Rotkin <valentina.rotkin@thenativeweb.io>
2019-11-19 11:07:29 +0100

    Move form components into forms category and add unit tests.

---
+++ test/unit/forms/FormActionsTests.tsx
@@ -0,0 +1,33 @@
+import { act } from '../../shared/act';
+import { assert } from 'assertthat';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { FormActions, ThemeProvider } from '../../../lib';
+
+suite('FormActions', (): void => {
+  let container: Element;
+
+  setup((): void => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  teardown((): void => {
+    document.body.removeChild(container);
+  });
+
+  test('renders although no property has been defined.', async (): Promise<void> => {
+    act((): void => {
+      ReactDOM.render(
+        <ThemeProvider>
+          <FormActions />
+        </ThemeProvider>,
+        container
+      );
+    });
+
+    const formActions = container.querySelector('div');
+
+    assert.that(formActions!.className).is.containing('FormActions');
+  });
+});
---
+++ test/unit/forms/FormTests.tsx
@@ -0,0 +1,59 @@
+import { act } from '../../shared/act';
+import { assert } from 'assertthat';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { submit } from '../../shared/eventDispatchers';
+import { Form, ThemeProvider } from '../../../lib';
+
+suite('Form', (): void => {
+  let container: Element;
+
+  setup((): void => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  teardown((): void => {
+    document.body.removeChild(container);
+  });
+
+  test('renders although no property has been defined.', async (): Promise<void> => {
+    act((): void => {
+      ReactDOM.render(
+        <ThemeProvider>
+          <Form />
+        </ThemeProvider>,
+        container
+      );
+    });
+
+    const form = container.querySelector('form');
+
+    assert.that(form!.className).is.containing('Form');
+  });
+
+  test('takes onSubmit function and runs it if submitted.', async (): Promise<void> => {
+    let submitted = false;
+
+    const onSubmit = (): void => {
+      submitted = true;
+    };
+
+    act((): void => {
+      ReactDOM.render(
+        <ThemeProvider>
+          <Form onSubmit={ onSubmit } />
+        </ThemeProvider>,
+        container
+      );
+    });
+
+    const form = container.querySelector('form');
+
+    act((): void => {
+      submit(form!);
+    });
+
+    assert.that(submitted).is.true();
+  });
+});
