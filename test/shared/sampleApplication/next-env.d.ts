58bf8a38d8aaa5e2b64ff2a5661400da5a1ebea7
Golo Roden <golo.roden@thenativeweb.io>
2019-11-22 22:20:41 +0100

    Add missing declaration file for reactElementToJsxString.

--- test/shared/sampleApplication/next-env.d.ts
+++ test/shared/sampleApplication/next-env.d.ts
@@ -1,6 +1,3 @@
-/* eslint-disable unicorn/filename-case */
-
 /* eslint-disable spaced-comment */
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-/* eslint-enable spaced-comment */
