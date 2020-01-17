80c19d7beb1a15be220f3f36c5f6ea2ef8f23239
Matthias Wagler <wagler@intuity.de>
2020-01-17 23:32:07 +0100

    Streamline docs for HorizontalBar.

--- lib/components/layout/HorizontalBar/Documentation.tsx
+++ lib/components/layout/HorizontalBar/Documentation.tsx
@@ -1,6 +1,7 @@
 import { ComponentPreview } from '../../../components/documentation/ComponentPreview';
 import { Headline, HorizontalBar, Paragraph } from '../../..';
-import React, { ReactElement } from 'react';
+import React, { ReactElement, Fragment } from 'react';
+import { Property } from '../../documentation/Property';
 
 const Documentation = (): ReactElement => (
   <React.Fragment>
@@ -8,67 +9,95 @@ const Documentation = (): ReactElement => (
 
     <Paragraph>
       The <code>HorizontalBar</code> can be used for building all kinds
-      of bars, e.g. a header or footer for a content panel. By default,
-      a <code>HorizontalBar</code> is optically separated by a lower border.
+      of bars, e.g. a header or footer element.
     </Paragraph>
 
     <ComponentPreview previewPadding={ false }>
-      <div>
-        <HorizontalBar>
-          This is a bar
+      <HorizontalBar>
+        This is a bar
+      </HorizontalBar>
+    </ComponentPreview>
+
+    <Headline level='2'>Setting the borders</Headline>
+
+    <Paragraph>
+      By default, a <code>HorizontalBar</code> is visually separated by a lower
+      border. Use the property <code>borderBottom</code> and <code>borderTop</code> to
+      adjust this behaviour.
+    </Paragraph>
+
+    <ComponentPreview>
+      <Fragment>
+        <Property name='borderBottom' value={ false } />
+        <HorizontalBar borderBottom={ false }>
+          This bar has no border
         </HorizontalBar>
-        <HorizontalBar>
-          <div style={{ flexGrow: 1 }}>Growing element</div>
-          <div style={{ flexGrow: 0 }}>Element with fix width</div>
+        <Property name='borderTop' value={ true } />
+        <HorizontalBar borderTop={ true }>
+          This bar has a border top as well as bottom
         </HorizontalBar>
-      </div>
+      </Fragment>
     </ComponentPreview>
 
     <Headline level='2'>Setting the background</Headline>
 
     <ComponentPreview previewPadding={ false }>
-      <div>
-        <HorizontalBar background='dark' borderBottom={ true }>
-          This is dark bar
-        </HorizontalBar>
-        <HorizontalBar background='dark' borderBottom={ true }>
-          This is dark bar
-        </HorizontalBar>
-      </div>
+      <HorizontalBar background='dark'>
+        This is a dark bar
+      </HorizontalBar>
     </ComponentPreview>
 
     <Headline level='2'>Sizing elements</Headline>
 
+    <Paragraph>
+      The children of a <code>HorizontalBar</code> will be rendered from left to
+      right, using only the minimum available horizontal space. By
+      setting the <code>flexGrow</code> style property, you can enable children
+      to occupy all the available horizontal space.
+    </Paragraph>
+
     <ComponentPreview previewPadding={ false }>
-      <div>
-        <HorizontalBar borderBottom={ true }>
+      <Fragment>
+        <HorizontalBar>
+          <div style={{ background: 'deeppink' }}>Element with auto width</div>
+          <div style={{ background: 'gray' }}>Another element with auto width</div>
+        </HorizontalBar>
+        <HorizontalBar>
           <div style={{ flexGrow: 1, background: 'deeppink' }}>Growing element</div>
           <div style={{ flexGrow: 0, background: 'gray' }}>Fixed width</div>
         </HorizontalBar>
-      </div>
+      </Fragment>
     </ComponentPreview>
 
     <Headline level='2'>Aligning elements</Headline>
 
+    <Paragraph>
+      The children of a <code>HorizontalBar</code> will be rendered from left
+      to right. Use the <code>align</code> property, to adjust this behaviour.
+    </Paragraph>
+
     <ComponentPreview previewPadding={ false }>
-      <div>
-        <HorizontalBar borderBottom={ true } align='left'>
-          <div style={{ background: 'deeppink' }}>1</div>
-          <div style={{ background: 'gray' }}>2</div>
+      <Fragment>
+        <HorizontalBar align='left'>
+          <div>1</div>
+          <div>2</div>
         </HorizontalBar>
-        <HorizontalBar borderBottom={ true } align='center'>
-          <div style={{ background: 'deeppink' }}>1</div>
-          <div style={{ background: 'gray' }}>2</div>
+
+        <HorizontalBar align='center'>
+          <div>1</div>
+          <div>2</div>
         </HorizontalBar>
-        <HorizontalBar borderBottom={ true } align='right'>
-          <div style={{ background: 'deeppink' }}>1</div>
-          <div style={{ background: 'gray' }}>2</div>
+
+        <HorizontalBar align='right'>
+          <div>1</div>
+          <div>2</div>
         </HorizontalBar>
-        <HorizontalBar borderBottom={ true } align='space-between'>
-          <div style={{ background: 'deeppink' }}>1</div>
-          <div style={{ background: 'gray' }}>2</div>
+
+        <HorizontalBar align='space-between'>
+          <div>1</div>
+          <div>2</div>
         </HorizontalBar>
-      </div>
+      </Fragment>
     </ComponentPreview>
   </React.Fragment>
 );
