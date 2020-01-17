69053374c4c451170c2528fbd224d5873b961bdb
Golo Roden <golo.roden@thenativeweb.io>
2020-01-17 21:22:46 +0100

    Fix file name rule for pages folder.

--- styleguide/pages/styles/creating-custom-components.tsx
+++ styleguide/pages/styles/creating-custom-components.tsx
@@ -1,5 +1,3 @@
-// Todo: Decide how to deal with filenames in kebap case in pages folder.
-// eslint-disable-next-line unicorn/filename-case
 import { Documentation } from '../../../lib/styles/Documentation';
 import React, { ReactElement } from 'react';
 
