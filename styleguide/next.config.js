3c81363a18f6f7d0cafd0e2277444f12a331ceed
Matthias Wagler <wagler@intuity.de>
2020-02-21 07:34:40 +0100

    Add analyse-bundle script.

--- styleguide/next.config.js
+++ styleguide/next.config.js
@@ -1,13 +1,21 @@
 const path = require('path');
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  // eslint-disable-next-line no-process-env
+  enabled: process.env.ANALYSE_BUNDLE === 'true'
+});
+
 const libraryEntryPointPath = path.resolve(__dirname, '..', 'lib');
 
-module.exports = {
+const nextConfiguration = {
   // We dont't want Next.js to create pages like components.html but components/index.html.
   exportTrailingSlash: true,
+
   webpack (configuration) {
     configuration.module.rules[0].include.push(libraryEntryPointPath);
 
     return configuration;
   }
 };
+
+module.exports = withBundleAnalyzer(nextConfiguration);
