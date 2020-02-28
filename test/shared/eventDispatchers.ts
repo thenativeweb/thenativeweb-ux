a016e5a08ee9c850732ed5b89725bbe00abd1cd7
Matthias Wagler <wagler@intuity.de>
2020-02-28 19:06:29 +0100

    Add unit tests for onClick and onMouseOver.

--- test/shared/eventDispatchers.ts
+++ test/shared/eventDispatchers.ts
@@ -2,11 +2,16 @@ const click = (element: HTMLElement): void => {
   element.dispatchEvent(new MouseEvent('click', { bubbles: true }));
 };
 
+const mouseover = (element: HTMLElement): void => {
+  element.dispatchEvent(new Event('mouseover', { bubbles: true }));
+};
+
 const submit = (element: HTMLElement): void => {
   element.dispatchEvent(new Event('submit', { bubbles: true }));
 };
 
 export {
   click,
+  mouseover,
   submit
 };
