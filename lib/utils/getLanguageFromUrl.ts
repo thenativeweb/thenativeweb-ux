769071927ccb1171053431dcf31413d0a70f1541
Matthias Wagler <wagler@intuity.de>
2020-02-28 11:41:36 +0100

    fix: Switch all configuration files to lowercase.

--- lib/utils/getLanguageFromUrl.ts
+++ lib/utils/getLanguageFromUrl.ts
@@ -3,7 +3,7 @@ import { Language } from '../types/Language';
 const getLanguageFromUrl = function (url: string): Language {
   const language = url.split('/').filter((item): boolean => item !== '')[0];
 
-  return language || 'en-US';
+  return language || 'en-us';
 };
 
 export { getLanguageFromUrl };
