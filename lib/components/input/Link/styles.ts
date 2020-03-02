5d28c495350774731490ed380f0f7c140f27c7c9
Matthias Wagler <wagler@intuity.de>
2020-03-02 12:03:30 +0100

    Remove font-weight and font-family from Link styles.

--- lib/components/input/Link/styles.ts
+++ lib/components/input/Link/styles.ts
@@ -5,8 +5,6 @@ export type LinkClassNames = 'Link';
 const styles = (theme: Theme): ComponentClassNames<LinkClassNames> => ({
   Link: {
     color: theme.color.brand.highlight,
-    fontWeight: 400,
-    fontFamily: theme.font.family.default,
     fontSize: 'inherit',
     textDecoration: 'none',
 
