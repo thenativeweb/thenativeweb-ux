808304fb01f3cb99637198a279ace5ed9914508e
Matthias Wagler <wagler@intuity.de>
2020-03-09 14:24:40 +0100

    Update pricacy policy page for styleguide.

--- styleguide/pages/en-us/privacy-policy.tsx
+++ styleguide/pages/en-us/privacy-policy.tsx
@@ -1,11 +1,229 @@
-import { Headline } from '../../../lib';
 import { Styleguide } from '../../layouts/Styleguide';
+import { Headline, Link, Paragraph } from '../../../lib';
 import React, { ReactElement } from 'react';
 
 /* eslint-disable react/jsx-child-element-spacing */
 export default (): ReactElement => (
   <Styleguide>
     <Headline>Privacy Policy</Headline>
+
+    <Headline level='2'>A. General</Headline>
+
+    <Paragraph>
+      The the native web GmbH operates this website. It is the interaction
+      styleguide for the native web GmbH, which offers consulting, training and
+      development services for JavaScript, Node.js and related technologies.
+    </Paragraph>
+
+    <Paragraph>
+      In order to operate this website we need to collect and process certain
+      personal data of our users. Data protection and data security are of
+      utmost importance to us. It goes without saying that we will fully comply
+      with all applicable statutory requirements and regulations. We will
+      collect, process and use personal data from users in connection with their
+      use of the websites only as described in this privacy policy.
+    </Paragraph>
+
+    <Paragraph>
+      Now, therefore, we will explain in detail in the following sections for
+      what purposes and to what extent your personal data will be collected,
+      processed and used.
+    </Paragraph>
+
+    <Headline level='2'>B. Responsibility</Headline>
+
+    <Paragraph>
+      We,<br /><br />
+
+      the native web GmbH<br /><br />
+
+      Hauptstraße 8<br />
+      79359 Riegel am Kaiserstuhl<br />
+      Germany<br /><br />
+
+      +49 177 3373175<br />
+      hello@thenativeweb.io<br /><br />
+
+      (hereinafter also being referred to as we, us) operate this website. We
+      are responsible for the personal data that is processed in connection
+      with this websites.<br /><br />
+
+      Our data protection officer is Stefan Brandys. He can be contacted via
+      email using <Link href='mailto:privacy@thenativeweb.io'>privacy@thenativeweb.io</Link>.
+    </Paragraph>
+
+    <Headline level='2'>C. What is personal data</Headline>
+
+    <Paragraph>
+      Personal data means any information relating to an identified or
+      identifiable natural person; an identifiable natural person is one who can
+      be identified, directly or indirectly, in particular by reference to an
+      identifier such as a name, an identification number, location data, an
+      online identifier or to one or more factors specific to the physical,
+      physiological, genetic, mental, economic, cultural or social identity of
+      that natural person.
+    </Paragraph>
+
+    <Headline level='2'>D. Collection, storage and use of personal data</Headline>
+
+    <Headline level='3'>1. Inventory and content data</Headline>
+
+    <Paragraph>
+      We do not collect or process any inventory or content data on this website.
+      Inventory data is all data that is necessary for the establishment, content
+      or modification of the contract in relation to the use of telemedia.
+      Content data include, without limitation, information you choose to
+      provide while using the websites (e.g. by completing your profile) or
+      information we require to be able to provide certain services to you.
+    </Paragraph>
+
+    <Headline level='3'>2. Log files and cookies</Headline>
+
+    <Paragraph>
+      Furthermore some data are generated whenever a user visits this website,
+      and collected and stored by us. We need to process these data to enable
+      users to use this website.
+    </Paragraph>
+
+    <Headline level='4'>2.1. Log files</Headline>
+
+    <Paragraph>
+      Our web servers do not store information when users visit our websites.
+    </Paragraph>
+
+    <Headline level='4'>2.2. Cookies</Headline>
+
+    <Paragraph>
+      This website does not use cookies. Cookies are text files containing small
+      amounts of information which are stored on your computer.
+    </Paragraph>
+
+    <Headline level='2'>E. Links to other websites</Headline>
+
+    <Paragraph>
+      This websites contains links to websites of other providers that are not
+      connected with us. After clicking a link, we do not have any influence on
+      the use of any transmitted personal data to the other provider
+      (i.e. IP adress or url on which the link is located), since the conduct
+      of third parties is naturally beyond our control. We cannot assume
+      responsibility for the processing of the personal data by third parties.
+    </Paragraph>
+
+    <Headline level='2'>F. Disclosure of data to third parties</Headline>
+
+    <Paragraph>
+      We only disclose personal data to third parties:<br /><br />
+
+      if and to the extent necessary for the performance of the contracts we
+      have with you or in order to enforce any claims we are entitled to,<br /><br />
+
+      if we are entitled or required by law or ordered by a court to disclose
+      such information, or<br /><br />
+
+      if you have given your consent to such disclosure.<br /><br />
+
+      In some cases we use external service providers which have been assigned
+      by us to process data on our behalf, subject to our instructions. Such
+      service providers will be assigned by us solely in writing and in
+      accordance with the strict specifications of the Federal Data Protection
+      Act (BDSG) as well as the General Data Protection Regulation (GDPR) and
+      may not use your data for any other purposes.
+    </Paragraph>
+
+    <Headline level='2'>G. Data security</Headline>
+
+    <Paragraph>
+      Security of your personal data is very important to us. We have adopted
+      technical and organisational measures that ensure that we as well as our
+      external service providers meet the applicable provisions relating to
+      data protection.
+    </Paragraph>
+
+    <Paragraph>
+      Our employees are contractually bound to observe the provisions of the
+      Telemedia Act (TMG) and the Federal Data Protection Act (BDSG) as well as
+      the General Data Protection Directive (GDPR) in the handling of data.
+    </Paragraph>
+
+    <Headline level='2'>H. Recipients of the personal data</Headline>
+
+    <Paragraph>
+      In order to operate this website, we use the following services, which
+      collect and process certain personal data of yours:
+    </Paragraph>
+
+    <Paragraph>
+      This website is hosted by Netlify, Inc. 610 22nd Street, Suite 315,
+      San Francisco, CA 94107, USA. Therefore when you visit this website,
+      your browser connects to a server at Netlify. This server stores some
+      information about the connection in a so-called log file. We have no
+      access to these log files. For more information on how Netlify processes
+      your data, please see <Link href='https://www.netlify.com/privacy/'>Netlify&apos; privacy policy</Link>.
+    </Paragraph>
+
+    <Paragraph>
+      This website use Google Fonts, a service by Google LLC, 1600 Amphitheatre
+      Parkway, Mountain View CA 94043, United States. Visiting our websites
+      discloses your IP address, the type of device you are using, your
+      operating system and browser, your language settings, and a variety of
+      additional information automatically sent by your browser to Google Fonts.
+      For further information see <Link href='https://policies.google.com/privacy?hl=en'>https://policies.google.com/privacy?hl=en</Link>.
+    </Paragraph>
+
+    <Headline level='2'>I. Further information and your rights</Headline>
+
+    <Paragraph>
+      You have the right to request confirmation whether we process any personal
+      data concerning you. In this case, you have a right of disclosure of such
+      personal data and, inter alia, to information on the processing purposes,
+      categories of personal data that are processed and the recipients or
+      categories of recipients to whom the personal data have been or will be
+      disclosed.
+    </Paragraph>
+
+    <Paragraph>
+      In addition, you can obtain the data that is the subject of processing in
+      a copy or a common electronic format. Furthermore, you can obtain the
+      relevant personal data in a structured, common and machine-readable format
+      and, if the legal conditions are met, you can transmit it to other
+      responsible persons. For inquiries please contact the address given in
+      section J.
+    </Paragraph>
+
+    <Paragraph>
+      You further have the right to have your personal data corrected, blocked,
+      deleted or to restrict the processing if the respective legal requirements
+      are met. In case we have stored any incorrect information about you,
+      please let us know and we will correct it without undue delay. For
+      corrections, please contact us at the address indicated in section J.
+    </Paragraph>
+
+    <Paragraph>
+      Personal data will be deleted if their collection or processing was
+      unlawful from the outset, if their processing or use proves to be
+      unlawful due to circumstances subsequently occurring or if we no longer
+      need to know such data to fulfil the purpose of their processing or use.
+      Only data we need to complete any tasks that remain outstanding or to
+      enforce our rights and claims, as well as data we are required to retain
+      by law will be excluded from deletion. However, such data will be blocked.
+    </Paragraph>
+
+    <Paragraph>
+      Furthermore, you have the right to lodge a complaint with a supervisory
+      authority, especially in the Member State of your residency, your
+      workplace or the place of the suspected violation or if you consider,
+      that the processing of the personal data concerning you is violating
+      applicable law.
+    </Paragraph>
+
+    <Headline level='2'>J. Contact for questions</Headline>
+
+    <Paragraph>
+      If you have any questions about our data protection procedures, please
+      contact us by email using <Link href='mailto:privacy@thenativeweb.io'>privacy@thenativeweb.io</Link>.<br /><br />
+
+      Last amended: March 9th, 2020
+    </Paragraph>
   </Styleguide>
 );
 /* eslint-eanble react/jsx-child-element-spacing */
