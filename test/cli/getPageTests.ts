0f9e87ceb3680a2e895b27a9b064d63c2acd940f
Golo Roden <golo.roden@thenativeweb.io>
2021-01-05 16:56:14 +0100

    Update getPageTests.ts

--- test/cli/getPageTests.ts
+++ test/cli/getPageTests.ts
@@ -2,7 +2,7 @@ import { assert } from 'assertthat';
 import { getPage } from 'lib/cli/verifyLinks/getPage';
 
 suite('getPage', (): void => {
-  test('is able to fetch from twitter.', async (): Promise<void> => {
+  test('is able to fetch from Twitter.', async (): Promise<void> => {
     await assert.that(async (): Promise<void> => {
       await getPage({ url: 'https://twitter.com/twitter' });
     }).is.not.throwingAsync();
